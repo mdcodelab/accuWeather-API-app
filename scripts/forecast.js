d0f6590e2751a45886e029a1a6b666a1985e3964
miha11-hash <d_mihaela@msn.com>
2023-01-11 01:02:32 +0200

    first commit

---
+++ scripts/forecast.js
@@ -0,0 +1,37 @@
+const key = 'YZx7Hdo2xgzwAPLPDma0h06kEqp0lBwS';  //this key allows us 50 requests a day (maximum)
+
+//get city information (Location Kw -> Text Search)
+
+const getCity = async (city)=> {
+const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';   //resurse url
+const query = `?apikey=${key}&q=${city}`;
+const request = await fetch(base + query);
+const data = await request.json();
+return data[0];
+}; 
+
+//get weather information (Current Conditions API)
+
+const getWeather = async(Key)=> {
+    const base = `http://dataservice.accuweather.com/currentconditions/v1/`;    //resurse url
+    const query = `${Key}?apikey=${key}`;
+    
+    const request = await fetch(base + query);
+    const data = await request.json();
+    return data[0];
+    };
+    
+
+// getCity('iasi').then(data=>{
+//     return getWeather(data.Key);
+// }).then (data=> {
+// console.log(data);
+// }).catch(err=>{
+// console.log(err);
+// });
+
+
+
+
+
+
