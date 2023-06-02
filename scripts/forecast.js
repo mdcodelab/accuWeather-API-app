66cf48f6ae2903c80796b3b4e4214435d5de588c
miha11-hash <d_mihaela@msn.com>
2023-06-02 03:45:19 +0300

    env file

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,26 +1,25 @@
-const key = 'YZx7Hdo2xgzwAPLPDma0h06kEqp0lBwS';  //this key allows us 50 requests a day (maximum)
+const key = process.env.ACCUEATHER_KEY;
 
 //get city information (Location Kw -> Text Search)
 
-const getCity = async (city)=> {
-const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';   //resurse url
-const query = `?apikey=${key}&q=${city}`;
-const request = await fetch(base + query);
-const data = await request.json();
-return data[0];
-}; 
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search"; //resurse url
+  const query = `?apikey=${key}&q=${city}`;
+  const request = await fetch(base + query);
+  const data = await request.json();
+  return data[0];
+};
 
 //get weather information (Current Conditions API)
 
-const getWeather = async(Key)=> {
-    const base = `http://dataservice.accuweather.com/currentconditions/v1/`;    //resurse url
-    const query = `${Key}?apikey=${key}`;
-    
-    const request = await fetch(base + query);
-    const data = await request.json();
-    return data[0];
-    };
-    
+const getWeather = async (Key) => {
+  const base = `http://dataservice.accuweather.com/currentconditions/v1/`; //resurse url
+  const query = `${Key}?apikey=${key}`;
+
+  const request = await fetch(base + query);
+  const data = await request.json();
+  return data[0];
+};
 
 // getCity('iasi').then(data=>{
 //     return getWeather(data.Key);
@@ -29,9 +28,3 @@ const getWeather = async(Key)=> {
 // }).catch(err=>{
 // console.log(err);
 // });
-
-
-
-
-
-
